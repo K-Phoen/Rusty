b32bfebc390289822f3c456bd2a2747cf4fa488b
Kévin Gomez <contact@kevingomez.fr>
2016-05-04 21:55:44 +0200

    Remove old test files

--- rusty_assert.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function rusty_assert($originalCode, $value, $msg = null)
-{
-    if (!$value) {
-        fprintf(STDERR, '[Rusty] Assertion failed: "%s"', $originalCode);
-        exit(1);
-    }
-}
