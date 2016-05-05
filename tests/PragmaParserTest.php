40f03c37409f2a6a9a9a35ccf7a91f9790dad818
Kévin Gomez <contact@kevingomez.fr>
2016-05-05 15:57:45 +0200

    Add a few tests for the PragmaParser class

---
+++ tests/PragmaParserTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Rusty\Tests;
+
+use Rusty\PragmaParser;
+
+class PragmaParserTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider inputProvider
+     */
+    public function testItParsesPragmaDirectives(string $input, array $expectedDirectives)
+    {
+        $parser = new PragmaParser();
+
+        $this->assertEquals($expectedDirectives, $parser->getPragmaDirectives($input));
+    }
+
+    public function inputProvider()
+    {
+        return [
+            [ '', [] ],
+
+            // known directives
+            [ 'ignore', [PragmaParser::IGNORE] ],
+            [ 'no_run', [PragmaParser::NO_RUN] ],
+            [ 'should_throw', [PragmaParser::SHOULD_THROW] ],
+
+            // unknown directives are silently ignored
+            [ 'unknown_directive', [] ],
+
+            // it makes no sense (for the moment), but several directives can be recognized
+            [ 'no_run should_throw', [PragmaParser::NO_RUN, PragmaParser::SHOULD_THROW] ],
+        ];
+    }
+}
