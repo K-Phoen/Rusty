f8d20a57f0bd7c241b280df70809842ebe702d63
Kévin Gomez <contact@kevingomez.fr>
2016-05-09 22:44:30 +0200

    Tweak a bit the messages in the ConsoleReporter

--- src/Reports/ConsoleReporter.php
+++ src/Reports/ConsoleReporter.php
@@ -27,24 +27,24 @@ class ConsoleReporter implements Reporter
             $this->output->writeln(sprintf(' → Found code sample in line %d', $report->getSample()->getLine()), OutputInterface::VERBOSITY_VERBOSE);
             $this->output->writeln($this->formatSample($report->getSample()), OutputInterface::VERBOSITY_VERY_VERBOSE);
         } else if ($report instanceof CodeSampleSkipped) {
-            $this->output->writeln(' <comment>⚐</comment> Skipped code sample', OutputInterface::VERBOSITY_VERBOSE);
+            $this->output->writeln(' <comment>⚐</comment> Skipped', OutputInterface::VERBOSITY_VERBOSE);
         } else if ($report instanceof CodeSampleLinted) {
-            $this->output->writeln(' <info>✔</info> Linted code sample', OutputInterface::VERBOSITY_VERBOSE);
+            $this->output->writeln(' <info>✔</info> No syntax error detected', OutputInterface::VERBOSITY_VERBOSE);
         } else if ($report instanceof CodeSampleLintFailure) {
             $sample = $report->getSample();
 
-            $this->output->writeln(sprintf(' <error>✘</error> Syntax error in code sample %s:%s', $sample->getFile()->getPathname(), $sample->getLine()), OutputInterface::VERBOSITY_NORMAL);
+            $this->output->writeln(sprintf(' <error>✘</error> Syntax error in %s:%s', $sample->getFile()->getPathname(), $sample->getLine()), OutputInterface::VERBOSITY_NORMAL);
             $this->output->writeln(sprintf('%s', $this->formatSample($sample)), OutputInterface::VERBOSITY_NORMAL);
 
             $message = explode("\n", trim(sprintf("[%s]\n%s", get_class($report->getError()), $report->getError()->getMessage())));
             $this->output->writeln($this->formatter->formatBlock($message, 'error', true), OutputInterface::VERBOSITY_NORMAL);
         } else if ($report instanceof SuccessfulExecution) {
-            $this->output->writeln(' <info>✔</info> Executed code sample', OutputInterface::VERBOSITY_VERBOSE);
+            $this->output->writeln(' <info>✔</info> Execution OK', OutputInterface::VERBOSITY_VERBOSE);
         } else if ($report instanceof ExecutionFailedAsExpected) {
-            $this->output->writeln(' <info>✔</info> Executed code sample failed as expected', OutputInterface::VERBOSITY_VERBOSE);
+            $this->output->writeln(' <info>✔</info> Execution failed as expected', OutputInterface::VERBOSITY_VERBOSE);
             $this->output->writeln($report->getResult()->getErrorOutput(), OutputInterface::VERBOSITY_VERY_VERBOSE);
         } else if ($report instanceof ExecutionShouldHaveFailed) {
-            $this->output->writeln(' <error>✘</error> Executed code sample was expected to fail but did not', OutputInterface::VERBOSITY_VERBOSE);
+            $this->output->writeln(' <error>✘</error> Execution was expected to fail but did not', OutputInterface::VERBOSITY_VERBOSE);
             $this->output->writeln($report->getResult()->getOutput(), OutputInterface::VERBOSITY_VERY_VERBOSE);
         } else if ($report instanceof ExecutionFailure) {
             $sample = $report->getSample();
