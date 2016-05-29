1f8bbd78d88755d91d079904f54d7986d6119fde
Kévin Gomez <contact@kevingomez.fr>
2016-05-29 17:54:25 +0200

    Add a logo in the cli app

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -8,10 +8,22 @@ use Rusty\Console\Command;
 
 class Application extends ConsoleApplication
 {
+    const LOGO = '__________                __
+\______   \__ __  _______/  |_ ___.__.
+ |       _/  |  \/  ___/\   __<   |  |
+ |    |   \  |  /\___ \  |  |  \___  |
+ |____|_  /____//____  > |__|  / ____|
+        \/           \/        \/     ';
+
     public function __construct()
     {
         parent::__construct('Rusty');
 
         $this->add(new Command\Check());
+    }
+
+    public function getHelp()
+    {
+        return self::LOGO . "\n" . parent::getHelp();
     }
 }
